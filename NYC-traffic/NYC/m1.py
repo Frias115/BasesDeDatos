b849c51ae1b73b34da9bb0e8e7a2f1d5b272ff20
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-05-18 14:46:10 +0200

    Parsing document but need to fix float error

--- NYC-traffic/NYC/m1.py
+++ NYC-traffic/NYC/m1.py
@@ -5,7 +5,29 @@
 import sys
 import string
 
-for line in sys.stdin:
-    firstLine = line.split(',')
-    print firstLine
-    break
+num_of_columns = 0
+primera_linea = True
+
+data = sys.stdin.readlines()
+print len(data)
+for line in data:
+    fila = line.split(',')
+    # Imprime la primera linea
+    if primera_linea:
+        firstLine = fila
+        primera_linea = False
+        print len(firstLine), firstLine
+    else:
+        pasa_chequeo = False
+        if len(fila) == len(firstLine):
+            pasa_chequeo = True
+        for element in range(0, len(fila)):
+            fila[element] = fila[element].lstrip().rstrip()
+            if element < 2 or element > 7:
+                if fila[element].replace('.', 'e').isdigit():
+                    pasa_chequeo = True
+                else:
+                    pasa_chequeo = False
+                    break
+        if pasa_chequeo:
+            print fila
